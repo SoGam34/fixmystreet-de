6dd5bedd150d47e815c7214d716f382da0b1e854
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2024-04-17 11:18:18 +0000

    Hinzufügen vom MapIt Ids und die Erhaltung
    Über https://global.mapit.mysociety.org/ kann man als json sich die Gebiete die in einem anderem Gebiet liegen herunterladen. Für Deutschland sind die Regirungsbezirke interresannt. Zum Rausfiltern der Interresanten Gebiete mit den für FMS relevanten Informationen kann man python3 Extractor.py -f dateiname.json aufrufen. Der Output landet in output.txt

---
+++ Organisatorisches/Extractor.py
@@ -0,0 +1,35 @@
+
+import json
+import argparse
+parser = argparse.ArgumentParser()                                               
+
+parser.add_argument("--file", "-f", type=str, required=True)
+args = parser.parse_args()
+
+input = open(args.file, 'r', encoding="utf-8")
+output = open('output.txt', 'w', encoding="utf-8")
+
+input = json.load(input)
+
+for x, obj in input.items():
+    if obj["type"] == 'O08':
+        output.write(str(obj["id"])+ ', #'+obj["name"]+'\n')
+
+for x, obj in input.items():
+    if obj["type"] == 'O06':
+        output.write(str(obj["id"])+ ', #'+obj["name"]+'\n')
+
+
+output.write('\n\n\n\n\n')
+
+
+for x, obj in input.items():
+    if obj["type"] == 'O08':
+        output.write(obj["name"]+'\n')
+
+for x, obj in input.items():
+    if obj["type"] == 'O06':
+        output.write(obj["name"]+'\n')
+
+output.close()
+
